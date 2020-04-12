cefc3c4d12f0908e96a7a8abc92dbb5673f79063
Ravinda Putra <rvnda.putra@gmail.com>
2020-04-12 18:42:32 +0700

    [rvndaputra] improvement promo page

--- pages/promo/[code].js
+++ pages/promo/[code].js
@@ -1,16 +1,72 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 import parser from "html-react-parser";
+import moment from "moment";
+
+import styles from "./PromoDetail.module.scss";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
 
 export default function Promo({ data }) {
   const router = useRouter();
 
   const promo = data.find((x) => x.code === router.query.code);
-  console.log(promo);
+  const otherPromo = data.slice(0, 2);
 
   return (
-    <div>
-      <h3>{promo.code}</h3>
-      <p>{parser(promo.description)}</p>
+    <div className={styles.PromoDetail}>
+      <Navbar />
+      <Header title="Promo" />
+      <main>
+        <section>
+          <div>
+            <img src={promo.image} alt="" />
+            <h3 className="is-size-4">{promo.title}</h3>
+            <p>
+              <b>{`KODE PROMO: ${promo.code}`}</b>
+            </p>
+            {parser(promo.description)}
+            <ol>
+              {promo.termsConditions.map((x, idx) => (
+                <li>{x}</li>
+              ))}
+            </ol>
+          </div>
+          <div>
+            <h3 className="is-size-5">Promo Lainnya</h3>
+            <div>
+              {otherPromo.map((promo) => (
+                <div key={promo.code} className="promo">
+                  <div
+                    className="image"
+                    style={{ backgroundImage: `url(${promo.image})` }}
+                  />
+                  <div className="detail">
+                    <h3 className="name is-size-6">{promo.title}</h3>
+                    <hr />
+                    <div>
+                      <span>
+                        <b>Masa Berlaku</b>
+                        <br />
+                        {`${moment(promo.period.start, "DD/MM/YYYY").format(
+                          "DD MMM"
+                        )} - ${moment(promo.period.end, "DD/MM/YYYY").format(
+                          "DD MMM YYYY"
+                        )}`}
+                      </span>
+                      <Link href="promo/[code]" as={`/promo/${promo.code}`}>
+                        <a className="ava-btn btn-primary">Lihat Detail</a>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+      </main>
+      <Footer />
     </div>
   );
 }
