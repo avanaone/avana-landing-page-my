5e6601e527c8a05471347bf5aef8c0204465dfc8
Ravinda Putra <rvnda.putra@gmail.com>
2020-04-13 09:42:51 +0700

    [rvndaputra] add promo page

--- pages/promo.js
+++ pages/promo.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Link from "next/link";
 import moment from "moment";
 
 import styles from "./scss/Promo.module.scss";
@@ -6,7 +7,6 @@ import styles from "./scss/Promo.module.scss";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Link from "next/link";
 
 const getPromo = import("../json/promo.json");
 
@@ -17,41 +17,50 @@ const Promo = () => {
     return setPromo(res.default);
   });
 
-  const promoUI = Array(9).fill(
-    promo.map((promo) => (
-      <div key={promo.code} className="promo">
-        <div
-          className="image"
-          style={{ backgroundImage: `url(${promo.image})` }}
-        />
-        <div className="detail">
-          <h3 className="name is-size-6">{promo.title}</h3>
-          <hr />
-          <div>
-            <span>
-              <b>Masa Berlaku</b>
-              <br />
-              {`${moment(promo.period.start, "DD/MM/YYYY").format(
-                "DD MMM"
-              )} - ${moment(promo.period.end, "DD/MM/YYYY").format(
-                "DD MMM YYYY"
-              )}`}
-            </span>
-            <Link href="promo/[code]" as={`/promo/${promo.code}`}>
-              <a className="ava-btn btn-primary">Lihat Detail</a>
-            </Link>
-          </div>
-        </div>
-      </div>
-    ))
-  );
-
   return (
     <div className={styles.Promo}>
       <Navbar />
       <Header title="Promo" />
       <main>
-        <section>{promoUI}</section>
+        <section>
+          {promo.map((promo) => (
+            <div key={promo.code} className="promo">
+              <div
+                className="image"
+                style={{ backgroundImage: `url(${promo.image})` }}
+              />
+              <div className="detail">
+                <div>
+                  <h3
+                    className="name is-size-6"
+                    title={promo.title}
+                  >{`${promo.title.slice(0, 75)}${
+                    promo.title.length > 75 ? " ..." : ""
+                  }`}</h3>
+                </div>
+                <hr />
+                <div>
+                  <span>
+                    <b>Masa Berlaku</b>
+                    <br />
+                    {promo.period.start !== promo.period.end
+                      ? `${moment(promo.period.start, "DD/MM/YYYY").format(
+                          "DD MMM"
+                        )} - ${moment(promo.period.end, "DD/MM/YYYY").format(
+                          "DD MMM YYYY"
+                        )}`
+                      : `${moment(promo.period.start, "DD/MM/YYYY").format(
+                          "DD MMM YYYY"
+                        )}`}
+                  </span>
+                  <Link href="promo/[code]" as={`/promo/${promo.code}`}>
+                    <a className="ava-btn btn-primary">Lihat Detail</a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
+        </section>
       </main>
       <Footer />
     </div>
