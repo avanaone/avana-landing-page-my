477a8157b1bdf75fdb4b99613a620fd7672934c9
Ravinda Putra <rvnda.putra@gmail.com>
2020-04-13 11:32:20 +0700

    [rvndaputra] fix dynamic routes

--- next.config.js
+++ next.config.js
@@ -1,19 +1,38 @@
+require("next/dynamic");
+
 module.exports = {
   exportTrailingSlash: true,
-  exportPathMap: function () {
-    return {
+  exportPathMap: async function () {
+    const paths = {
       "/": { page: "/" },
-      "/price": {page: "/price"},
-      "/event": {page: "/event"},
-      "/event/1": {page: "/event/[id]", query: { id: '1' }},
-      "/about-us": {page: "/about-us"},
-      "/promo": {page: "/promo"},
-      "/promo/PROMOCERIA": {page: "/promo/[code]", query: {code: 'PROMOCERIA'}},
-      "/packages-detail": {page: "/packages-detail"},
-      "/dashboard": {page: "/dashboard"},
-      "/avachat": {page: "/avachat"},
-      "/webstore": {page: "/webstore"},
-      "/reseller": {page: "/reseller"}
+      "/about-us": { page: "/about-us" },
+      "/price": { page: "/price" },
+      "/packages-detail": { page: "/packages-detail" },
+      "/event": { page: "/event" },
+      "/promo": { page: "/promo" },
+      "/dashboard": { page: "/dashboard" },
+      "/avachat": { page: "/avachat" },
+      "/webstore": { page: "/webstore" },
+      "/reseller": { page: "/reseller" },
     };
+
+    const getPromo = await require("./json/promo.json");
+    const getEvents = await require("./json/event.json");
+
+    getPromo.map((promo) => {
+      paths[`promo/${promo.code}`] = {
+        page: "/promo/[code]",
+        query: { code: promo.code },
+      };
+    });
+
+    getEvents.map((event) => {
+      paths[`event/${event.id}`] = {
+        page: "/event/[id]",
+        query: { id: event.id },
+      };
+    });
+
+    return paths;
   },
 };
