3d414c2ec29849f463fbe1bd5797230487fee4f3
Nurwin <nurwin.jtk10@gmail.com>
2020-04-13 10:09:18 +0700

    add routes export

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ module.exports = {
   exportPathMap: function () {
     return {
       "/": { page: "/" },
+      "/price": {page: "/price"},
+      "/event": {page: "/event"},
+      "/event/[id]": {page: "/event/[id]"},
+      "/about-us": {page: "/about-us"},
+      "/promo": {page: "/promo"},
+      "/promo/[code]": {page: "/promo/[code]"},
+      "/packages-detail": {page: "/packages-detail"},
+      "/dashboard": {page: "/dashboard"},
+      "/avachat": {page: "/avachat"},
+      "/webstore": {page: "/webstore"},
+      "/reseller": {page: "/reseller"}
     };
   },
 };
