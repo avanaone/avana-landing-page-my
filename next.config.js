f4e6cf6fac848779951405a8129a8c80534835d8
Ravinda Putra <rvnda.putra@gmail.com>
2020-04-28 08:24:11 +0700

    [rvndaputra] fix paths

--- next.config.js
+++ next.config.js
@@ -14,29 +14,31 @@ const nextConfig = {
       '/price': { page: '/price' },
       '/packages-detail': { page: '/packages-detail' },
       '/event': { page: '/event' },
+      '/event/:id': { page: '/event/[id]' },
       '/promo': { page: '/promo' },
+      '/promo/:code': { page: '/promo/[code]' },
       '/dashboard': { page: '/dashboard' },
       '/avachat': { page: '/avachat' },
       '/webstore': { page: '/webstore' },
       '/reseller': { page: '/reseller' },
     };
 
-    const getPromo = await require('./json/promo.json');
-    const getEvents = await require('./json/event.json');
+    // const getPromo = await require('./json/promo.json');
+    // const getEvents = await require('./json/event.json');
 
-    getPromo.map((promo) => {
-      paths[`promo/${promo.code}`] = {
-        page: 'promo/[code]',
-        query: { code: promo.code },
-      };
-    });
+    // getPromo.map((promo) => {
+    //   paths[`promo/${promo.code}`] = {
+    //     page: 'promo/[code]',
+    //     query: { code: promo.code },
+    //   };
+    // });
 
-    getEvents.map((event) => {
-      paths[`event/${event.id}`] = {
-        page: 'event/[id]',
-        query: { id: event.id },
-      };
-    });
+    // getEvents.map((event) => {
+    //   paths[`event/${event.id}`] = {
+    //     page: 'event/[id]',
+    //     query: { id: event.id },
+    //   };
+    // });
 
     return paths;
   },
