6cb114359b2b2658dc7782ee158d9d2d30438ead
Stanley Hoei <>
2020-05-15 23:07:09 +0700

    swap position promo

--- pages/promo.js
+++ pages/promo.js
@@ -28,18 +28,18 @@ const Promo = () => {
         <Header title='Promo' />
         <main>
           <section className="section-title">
-            <h3>Program</h3>
-          </section>
-          <section>
-            {promos.filter((x) => !x.isPromo ).map((promo) => (
-              <PromoCard key={promo.code} promo={promo} />
-            ))}
-          </section>
-          <section className="section-title">
             <h3>Promo</h3>
           </section>
           <section>
             {promos.filter((x) => x.isPromo ).map((promo) => (
+              <PromoCard key={promo.code} promo={promo} />
+            ))}
+          </section>
+          <section className="section-title">
+            <h3>Program</h3>
+          </section>
+          <section>
+            {promos.filter((x) => !x.isPromo ).map((promo) => (
               <PromoCard key={promo.code} promo={promo} />
             ))}
           </section>
