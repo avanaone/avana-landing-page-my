6f7098d89c02ef971037ae0e2753c21982cc0f62
Stanley Hoei <>
2020-07-02 10:09:21 +0700

    hide partner logos

--- pages/index.js
+++ pages/index.js
@@ -496,13 +496,13 @@ const Home = () => {
           <section id='support'>
             <h2 className='is-size-4'>{cw.support ? cw.support : 'loading'}</h2>
             <div>
-              <img
+              {/* <img
                 srcSet={
                   require('../public/assets/images/Partner-logos.png?resize?webp')
                     .srcSet
                 }
                 alt='Partner Logos'
-              />
+              /> */}
               {/* <div className='border' />
               <img
                 srcSet={
