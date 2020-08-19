7ce06a43804a4fbe5c7b14dcd61978d3e398a6e9
Hasbi Musaddad <musaddadhasbi@gmail.com>
2020-08-19 17:28:58 +0700

    [hasbi] change store.avana.asia into app.avana.asia

--- components/Feature.js
+++ components/Feature.js
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import Head from 'next/head';
+import React, { useState, useEffect } from "react";
+import Head from "next/head";
 
-import Navbar from './Navbar';
-import Header from './Header';
-import Footer from './Footer';
+import Navbar from "./Navbar";
+import Header from "./Header";
+import Footer from "./Footer";
 
-import { LinkButton } from './Button';
+import { LinkButton } from "./Button";
 
-const Features = import('../json/features.json');
+const Features = import("../json/features.json");
 
 export default function Feature(props) {
   const { id, title, link, CS, className, showVideo, isAnchor } = props;
-  const [lang, setLang] = useState('en');
+  const [lang, setLang] = useState("en");
   const [features, setFeatures] = useState([]);
 
   useEffect(() => {
-    if (localStorage.getItem('lang')) {
+    if (localStorage.getItem("lang")) {
       // console.log('start', localStorage.getItem('lang'));
-      setLang(localStorage.getItem('lang'));
+      setLang(localStorage.getItem("lang"));
     } else {
-      localStorage.setItem('lang', 'en');
+      localStorage.setItem("lang", "en");
     }
   }, []);
 
@@ -31,17 +31,17 @@ export default function Feature(props) {
   }, [lang]);
 
   return (
-    <div className='features'>
+    <div className="features">
       <Head>
         <title>{`${title} • AVANA`}</title>
       </Head>
       <Navbar />
       <Header
         title={
-          lang === 'bm' && title === 'Webstore'
-            ? 'Kedai Online'
-            : lang === 'bm' && title === 'Reseller'
-            ? 'Sales Agent'
+          lang === "bm" && title === "Webstore"
+            ? "Kedai Online"
+            : lang === "bm" && title === "Reseller"
+            ? "Sales Agent"
             : title
         }
       />
@@ -49,20 +49,20 @@ export default function Feature(props) {
         {showVideo ? (
           <>
             <div>
-              <h3 className='video-headline'>
+              <h3 className="video-headline">
                 “
-                {lang === 'en'
-                  ? 'Convenient and hassle-free live session with AVANA'
-                  : 'Pengalaman sesi LIVE bersama AVANA yang MUDAH dan SELESA!'}
+                {lang === "en"
+                  ? "Convenient and hassle-free live session with AVANA"
+                  : "Pengalaman sesi LIVE bersama AVANA yang MUDAH dan SELESA!"}
                 ”
               </h3>
             </div>
-            <section id='section-1' className='feature'>
+            <section id="section-1" className="feature">
               <div>
                 <iframe
-                  src='https://www.youtube.com/embed/_4BQZP5nIKo'
-                  frameBorder='0'
-                  allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+                  src="https://www.youtube.com/embed/_4BQZP5nIKo"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                   allowFullScreen
                   style={{
                     width: `600px`,
@@ -75,71 +75,71 @@ export default function Feature(props) {
             </section>
           </>
         ) : (
-          ''
+          ""
         )}
         {features.map((feature, idx) => (
           <section
             key={idx}
-            className={`feature ${className ? className : ''}`}
-            id={isAnchor ? `${feature.anchor}` : ''}
+            className={`feature ${className ? className : ""}`}
+            id={isAnchor ? `${feature.anchor}` : ""}
           >
-            <div className='description'>
-              <h2 className='is-size-4'>{feature.title}</h2>
+            <div className="description">
+              <h2 className="is-size-4">{feature.title}</h2>
               <p>{feature.description}</p>
             </div>
-            <div className='feature-img'>
+            <div className="feature-img">
               <img
                 srcSet={
                   require(`../public/assets/images/${feature.image}?resize?webp`)
                     .srcSet
                 }
-                alt=''
+                alt=""
               />
             </div>
           </section>
         ))}
-        <section className='trial'>
-          <h2 className='is-size-4'>
+        <section className="trial">
+          <h2 className="is-size-4">
             {CS
-              ? lang === 'en'
+              ? lang === "en"
                 ? `Interested with ${title}?`
                 : `Berminat dengan ${title}?`
-              : lang === 'en'
-              ? 'Try AVANA 14 days for FREE!'
-              : 'Cuba AVANA selama 14 hari secara PERCUMA!'}
+              : lang === "en"
+              ? "Try AVANA 14 days for FREE!"
+              : "Cuba AVANA selama 14 hari secara PERCUMA!"}
             {/* { lang === 'en' ? 'Try AVANA 14 days for FREE!' : 'Cuba AVANA selama 14 hari secara PERCUMA!'} */}
           </h2>
-          {title === 'Whatsapp Commerce' ? (
-            <LinkButton className='btn-primary'>
-              {lang === 'en' ? 'Coming Soon' : 'Akan Datang'}
+          {title === "Whatsapp Commerce" ? (
+            <LinkButton className="btn-primary">
+              {lang === "en" ? "Coming Soon" : "Akan Datang"}
             </LinkButton>
-          ) : title === 'AVAChat' ? (
+          ) : title === "AVAChat" ? (
             <LinkButton
-              href={'https://forms.gle/uNDvYYLs2diJmr3WA'}
-              target='_blank'
-              className='btn-primary'
+              href={"https://forms.gle/uNDvYYLs2diJmr3WA"}
+              target="_blank"
+              className="btn-primary"
             >
-              {lang === 'en' ? 'Coming Soon' : 'Akan Datang'}
+              {lang === "en" ? "Coming Soon" : "Akan Datang"}
             </LinkButton>
           ) : (
             <LinkButton
               href={
                 CS
-                  ? lang === 'en' && title === 'AVAChat'
+                  ? lang === "en" && title === "AVAChat"
                     ? link[0]
                     : link[1]
-                  : 'https://store.avana.asia/'
+                  : "https://app.avana.asia/"
               }
-              target='__blank'
-              className='btn-primary'
+              target="__blank"
+              className="btn-primary"
             >
               {CS
-                ? lang === 'en'
-                  ? 'Contact Us'
-                  : 'Hubungi Kami'
-                : lang === 'en'
-                ? 'Try for FREE'
-                : 'Coba Gratis'}
+                ? lang === "en"
+                  ? "Contact Us"
+                  : "Hubungi Kami"
+                : lang === "en"
+                ? "Try for FREE"
+                : "Coba Gratis"}
               {/* {lang === 'en' ? 'Sign Up For Free' : 'DAFTAR SEKARANG'} */}
             </LinkButton>
           )}
