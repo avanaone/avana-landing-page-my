3dc4b4abd65c3ff0d030f0c5c857ddcaa0bc8304
Stanley Hoei <stanleyhoei@yahoo.com>
2020-09-04 16:11:16 +0700

    hotfix gtm position

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,17 @@ class MyDocument extends Document {
       <Html lang='id'>
         <Head>
           <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-T54DKZ6');
+          `,
+            }}
+          />
+          <script
             async=''
             charset='utf-8'
             src='https://cdn.zopim.com/?119GyjFZxz8YGSLmizOWVzpwS1HT3EEk'
@@ -71,17 +82,6 @@ class MyDocument extends Document {
             async=''
             src='https://connect.facebook.net/en_US/fbevents.js'
           ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-T54DKZ6');
-          `,
-            }}
-          />
         </Head>
         <body>
           <noscript
