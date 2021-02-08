bc53420cab988ce68b7411a26ac1ba93c8231812
feezixora <feezixora@gmail.com>
2021-02-08 10:47:37 +0800

    Activate Reseller Page

---
+++ pages/reseller.js
@@ -0,0 +1,16 @@
+import Feature from '../components/Feature';
+
+import ContainerAnalytic from '../components/AnalyticContainer';
+
+const Reseller = () => (
+  <ContainerAnalytic>
+    <Feature
+      id='reseller'
+      title='Reseller'
+      className='flex-d-row-reverse'
+      isAnchor
+    />
+  </ContainerAnalytic>
+);
+
+export default Reseller;
