0bb82b52502c551b908019b6103fec10a838de36
mrneverflatz <mrneverflatz@gmail.com>
2021-02-09 17:39:13 +0800

    fix toaster user proof

--- pages/_app.js
+++ pages/_app.js
@@ -1,120 +1,134 @@
-import { useEffect } from 'react';
-import Head from 'next/head';
+import { useEffect, useState, useCallback } from "react";
+import Head from "next/head";
 
-import 'bulma-scss/bulma.scss';
-import './scss/styles.scss';
-import './scss/Career.module.scss';
+import "bulma-scss/bulma.scss";
+import "./scss/styles.scss";
+import "./scss/Career.module.scss";
 // import "../components/Slider/Slider.scss";
+import Toaster from "../components/Toaster";
 
 export default function App({ Component, pageProps }) {
+  const [subscribers, setSubscribers] = useState([]);
+
+  const fetchSubscribers = useCallback(async () => {
+    const res = await fetch(
+      "https://api.avana.asia/recentSignUps?days=30&limit=5&country=Malaysia"
+    );
+    const subscribers = await res.json();
+
+    setSubscribers(subscribers);
+  }, [setSubscribers]);
+
   useEffect(() => {
-    localStorage.setItem('lang', localStorage.lang ? localStorage.lang : 'en');
+    localStorage.setItem("lang", localStorage.lang ? localStorage.lang : "en");
+    fetchSubscribers();
   }, []);
   return (
     <>
       <Head>
-        <meta charSet='UTF-8' />
-        <meta httpEquiv='X-UA-Compatible' content='IE=edge,chrome=1' />
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         {/* Primary Meta Tags */}
         <title>
           AVANA - The best social commerce solution for your business
         </title>
         <meta
-          name='title'
-          content='AVANA - The best social commerce solution for your business'
+          name="title"
+          content="AVANA - The best social commerce solution for your business"
         />
         <meta
-          name='description'
-          content='Our commerce platform helps automate your business so that you can make money with ease'
+          name="description"
+          content="Our commerce platform helps automate your business so that you can make money with ease"
         />
         {/* Open Graph / Facebook */}
-        <meta property='og:type' content='website' />
-        <meta property='og:url' content='https://avana.asia/' />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://avana.asia/" />
         <meta
-          property='og:title'
-          content='AVANA - The best social commerce solution for your business'
+          property="og:title"
+          content="AVANA - The best social commerce solution for your business"
         />
         <meta
-          property='og:description'
-          content='Our commerce platform helps automate your business so that you can make money with ease'
+          property="og:description"
+          content="Our commerce platform helps automate your business so that you can make money with ease"
         />
-        <meta property='og:image' content='/assets/images/meta-image.png' />
+        <meta property="og:image" content="/assets/images/meta-image.png" />
         {/* Twitter */}
-        <meta property='twitter:card' content='summary_large_image' />
-        <meta property='twitter:url' content='https://avana.asia/' />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://avana.asia/" />
         <meta
-          property='twitter:title'
-          content='AVANA - The best social commerce solution for your business'
+          property="twitter:title"
+          content="AVANA - The best social commerce solution for your business"
         />
         <meta
-          property='twitter:description'
-          content='Our commerce platform helps automate your business so that you can make money with ease'
+          property="twitter:description"
+          content="Our commerce platform helps automate your business so that you can make money with ease"
         />
         <meta
-          property='twitter:image'
-          content='/assets/images/meta-image.png'
+          property="twitter:image"
+          content="/assets/images/meta-image.png"
         />
         <meta
-          property='twitter:image'
-          content='/assets/images/meta-image.png'
+          property="twitter:image"
+          content="/assets/images/meta-image.png"
         />
         <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/assets/images/favicons/apple-touch-icon.png'
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/assets/images/favicons/apple-touch-icon.png"
         />
         <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='/assets/images/favicons/favicon-32x32.png'
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/images/favicons/favicon-32x32.png"
         />
         <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='/assets/images/favicons/favicon-16x16.png'
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/images/favicons/favicon-16x16.png"
         />
-        <link rel='manifest' href='/assets/images/favicons/manifest.json' />
+        <link rel="manifest" href="/assets/images/favicons/manifest.json" />
         <link
-          rel='mask-icon'
-          href='/assets/images/favicons/safari-pinned-tab.svg'
-          color='#5bbad5'
+          rel="mask-icon"
+          href="/assets/images/favicons/safari-pinned-tab.svg"
+          color="#5bbad5"
         />
-        <meta name='msapplication-TileColor' content='#da532c' />
-        <meta name='theme-color' content='#ffffff' />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
         <meta
-          name='viewport'
-          content='width=device-width, initial-scale=1, shrink-to-fit=no'
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <meta httpEquiv='cache-control' content='max-age=2592000' />
-        <meta httpEquiv='expires' content='0' />
-        <meta httpEquiv='pragma' content='no-cache' />
-        <meta httpEquiv='refresh' />
+        <meta httpEquiv="cache-control" content="max-age=2592000" />
+        <meta httpEquiv="expires" content="0" />
+        <meta httpEquiv="pragma" content="no-cache" />
+        <meta httpEquiv="refresh" />
         {/* <link
           rel='stylesheet'
           href='https://cdnjs.cloudflare.com/ajax/libs/bulma/0.8.2/css/bulma.min.css'
           async
         /> */}
         <link
-          rel='preconnect'
-          href='https://www.google-analytics.com'
-          as='script'
-          crossOrigin='anonymous'
+          rel="preconnect"
+          href="https://www.google-analytics.com"
+          as="script"
+          crossOrigin="anonymous"
         />
         <link
-          rel='preconnect'
-          href='https://static.doubleclick.net'
-          as='script'
+          rel="preconnect"
+          href="https://static.doubleclick.net"
+          as="script"
         />
         <link
-          rel='preconnect'
-          href='https://googleads.g.doubleclick.net'
-          as='script'
-          crossOrigin='anonymous'
+          rel="preconnect"
+          href="https://googleads.g.doubleclick.net"
+          as="script"
+          crossOrigin="anonymous"
         />
       </Head>
       <Component {...pageProps} />
+      <Toaster subscribers={subscribers} />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import fetch from "isomorphic-unfetch";
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Toaster from "../components/Toaster";
 import ContainerAnalytic from "../components/AnalyticContainer";
 import { LinkButton } from "../components/Button";
 // import Slider from "../components/Slider";
@@ -20,7 +19,7 @@ const getCw = import("../json/copywriting.json");
 // const getPromos = import('../json/promo.json');
 const getEvents = import("../json/event.json");
 
-const Home = ({ subscribers }) => {
+const Home = () => {
   const [lang, setLang] = useState("en");
   const [cw, setCw] = useState([]);
   const [isUtm, setIsUtm] = useState(false);
@@ -792,29 +791,10 @@ const Home = ({ subscribers }) => {
             </div>
           </div>
         </main>
-        <Toaster subscribers={subscribers} />
         <Footer />
       </div>
     </ContainerAnalytic>
   );
 };
-
-export async function getStaticProps() {
-  // Call an external API endpoint to get latest subscribers.
-  // You can use any data fetching library
-  const res = await fetch(
-    "https://api.avana.asia/recentSignUps?days=30&limit=5&country=Malaysia"
-  );
-  const subscribers = await res.json();
-  // console.log('stan', subscribers);
-
-  // By returning { props: subscribers }, the Home component
-  // will receive `subscribers` as a prop at build time
-  return {
-    props: {
-      subscribers,
-    },
-  };
-}
 
 export default Home;
